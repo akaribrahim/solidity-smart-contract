2c0445348a1f81e611fe9eed651ee52e477f0f9e
akaribrahim <bilalcanakar@gmail.com>
2020-12-20 00:28:03 +0300

    last functions

--- taxi.sol
+++ taxi.sol
@@ -1,12 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.7.0;
 
 contract Taxi {
     
     address managerAddress;
     address payable carDealerAddress;
-    uint fixedExpenses;
+    uint carExpenses;
     uint participationFee;
     uint participantsCount;
+    
+    uint driverAccount;
+    uint driverLastPaymentDay;
+    bool isDriverSalaryPaid;
+    
+    uint carExpensesLastPaymentDay;
+    bool isCarExpensesPaid;
+    
+    uint profit;
+    uint profitDividendLastPaymentDay;
     
     struct TaxiDriver {
         address payable driverAddress;
@@ -19,30 +30,44 @@ contract Taxi {
         uint proposeTime;
         uint validTime;
         uint approvalState;
+        bool isCarPurchased;
+        uint sellCarApprovalState;
     }
     
-    ProposedCar proposedCar;
+    ProposedCar public proposedCar;
     TaxiDriver proposedDriver;
-    TaxiDriver settedDriver;
+    TaxiDriver public settedDriver;
+    
+    // Each participant is associated with an index when added. 
+    //In this way, the balance of all of them is reached when necessary.
+    uint[] public participantsBalances;    
+    mapping(address => uint) public participantIndex;  
     mapping(address => bool) carApproves;
     mapping(address => bool) driverApproves;
-    mapping(address => uint) participants;
+    mapping(address => bool) sellProposalApproves;
+    mapping(address => bool) public isParticipant;
+    mapping(address => uint) participantsAccounts;
     
     
     
     constructor () {
         managerAddress = msg.sender;
-        fixedExpenses = 2 ether;
+        carExpenses = 2 ether;
         participationFee = 10 ether;
+        participantsBalances.push(0);
     }
     
     
+
     function join() external payable {
         require(msg.value == participationFee, "You must pay exactly 10 ether to join contract");
-        if(participants[msg.sender] != 0){
+        if(isParticipant[msg.sender]){
             revert("You already join the contract");
         }
-        participants[msg.sender] += participationFee;
+        
+        participantIndex[msg.sender] = participantsBalances.length;
+        participantsBalances.push(0);
+        isParticipant[msg.sender] = true;
         participantsCount += 1;
     }
     function contractBalance() external view returns(uint){
@@ -54,40 +79,81 @@ contract Taxi {
     }
     function carProposeToBusiness(uint carID, uint price, uint validTime) public {
         require(msg.sender == carDealerAddress, "This function can be called from car dealer only.");
-        proposedCar = ProposedCar(carID, price, block.timestamp, validTime, 0);
+        proposedCar = ProposedCar(carID, price, block.timestamp, validTime, 0, false, 0);
     }
     function approvePurchaseCar() external {
         require(!carApproves[msg.sender], "You already approved the proposed car.");
-        if(participants[msg.sender] == 0){
+        if(!isParticipant[msg.sender]){
             revert("You must participate the contract for this action.");
+        }
+        else if(proposedCar.price == 0){
+            revert("There is no car to approve");
         }
         carApproves[msg.sender] = true;
         proposedCar.approvalState += 1;
     }
     function purchaseCar() external {
         require(msg.sender == managerAddress, "This function can be called from manager only.");
-        if(proposedCar.approvalState > participantsCount / 2){
-            carDealerAddress.transfer(proposedCar.price * 1 ether);
-        }
-        else if(proposedCar.price == 0){
+        if(proposedCar.price == 0){
             revert("There is no proposed car to purchase");
         }
+        else if(proposedCar.approvalState > participantsCount / 2){
+            if(proposedCar.proposeTime + proposedCar.validTime > block.timestamp){
+                carDealerAddress.transfer(proposedCar.price * 1 ether);
+                proposedCar.isCarPurchased = true;
+            }
+            else{
+                revert("Valid Time exceed.");
+            }
+        }
         else{
-            revert("Valid Time exceed or approvalState is less than half of the participants");
+            revert("approvalState is less than half of the participants");
         }
     }
     function repurchaseCarPropose(uint carID, uint price, uint validTime) external {
         require(msg.sender == carDealerAddress, "This function can be called from car dealer only.");
-        carProposeToBusiness(carID, price, validTime);
+        if(proposedCar.isCarPurchased){
+            proposedCar = ProposedCar(carID, price, block.timestamp, validTime, 0, proposedCar.isCarPurchased, 0);
+        }
+        else{
+            revert("There is no car to sell");
+        }
     }
-    //ApproveSellProposal:      Repurchasecar:
+    function approveSellProposal() external {
+        require(!sellProposalApproves[msg.sender], "You already approved the sell proposal.");
+        if(!isParticipant[msg.sender]){
+            revert("You must participate the contract for this action.");
+        }
+        else if(!proposedCar.isCarPurchased){
+            revert("There is no car to sell");
+        }
+        sellProposalApproves[msg.sender] = true;
+        proposedCar.sellCarApprovalState += 1;
+    }
+    function repurchaseCar() external {
+        require(msg.sender == carDealerAddress, "This function can be called from car dealer only.");
+        if(!proposedCar.isCarPurchased){
+            revert("There is no car to sell.");
+        }
+        else if(proposedCar.sellCarApprovalState > participantsCount / 2){
+            if(proposedCar.proposeTime + proposedCar.validTime > block.timestamp){
+                carDealerAddress.transfer(proposedCar.price * 1 ether);
+            }
+            else{
+                revert("Valid Time exceed.");
+            }
+        }
+        else{
+            revert("approvalState is less than half of the participants");
+        }
+    }
     function proposeDriver(address payable driverAddress, uint salary) external {
         require(msg.sender == managerAddress, "This function can be called from manager only.");
         proposedDriver = TaxiDriver(driverAddress, salary, 0);
     }
     function approveDriver() external {
         require(!driverApproves[msg.sender], "You already approved the proposed driver.");
-        if(participants[msg.sender] == 0){
+        if(!isParticipant[msg.sender]){
             revert("You must participate the contract for this action.");
         }
         driverApproves[msg.sender] = true;
@@ -96,9 +162,9 @@ contract Taxi {
     function setDriver() external {
         require(msg.sender == managerAddress, "This function can be called from manager only.");
         if(proposedDriver.approvalState > participantsCount / 2){
-            settedDriver = proposedDriver;
+            settedDriver = TaxiDriver(proposedDriver.driverAddress, proposedDriver.salary, 0);
         }
-        else if(proposedCar.price == 0){
+        else if(proposedDriver.salary == 0){
             revert("There is no proposed driver to set");
         }
         else{
@@ -113,5 +179,81 @@ contract Taxi {
         settedDriver.driverAddress.transfer(settedDriver.salary * 1 ether);
         settedDriver = TaxiDriver(address(0), 0, 0);
         proposedDriver = TaxiDriver(address(0), 0, 0);
+    }
+    function payTaxiCharge() external payable{
+        // Ether cinsinden
+        require(settedDriver.salary != 0 || proposedCar.isCarPurchased,
+        "We don't have a working taxi right now. Therefore, you cannot have used the taxi.");
+    }
+    
+    modifier driverSalaryDay {
+        if (driverLastPaymentDay != 0) { // 0 ise ilk maaşıdır. Değilse en son ne zaman ödendiğine bak.
+            require(block.timestamp - driverLastPaymentDay > 30 days,
+                "30 days have not passed since the last payment."
+            );
+        }
+        _;
+    }
+    function releaseSalary() external driverSalaryDay{
+        require(msg.sender == managerAddress, "This function can be called from manager only.");
+        driverLastPaymentDay = block.timestamp;
+        isDriverSalaryPaid = true;
+        driverAccount += settedDriver.salary;
+    }
+    function getSalary() external {
+        require(msg.sender == settedDriver.driverAddress, "This function can be called from driver only.");
+        if(driverAccount == 0){
+            revert("There is no money in driver account");
+        }
+        settedDriver.driverAddress.transfer(driverAccount * 1 ether);
+        driverAccount = 0;
+    }
+    
+    modifier carExpensesDay {
+        if (carExpensesLastPaymentDay != 0) {
+            require(block.timestamp - driverLastPaymentDay > 180 days,
+                "6 months have not passed since the car expenses payment."
+            );
+        }
+        _;
+    }
+    function payCarExpenses() external carExpensesDay {
+        require(msg.sender == managerAddress, "This function can be called from manager only.");
+        carExpensesLastPaymentDay = block.timestamp;
+        isCarExpensesPaid = true;
+        carDealerAddress.transfer(carExpenses);
+    }
+    
+    modifier profitDividendDay {
+        if (profitDividendLastPaymentDay != 0) {
+            require(block.timestamp - driverLastPaymentDay > 180 days,
+                "6 months have not passed since the last profit distribution."
+            );
+        }
+        _;
+    }
+    function payDividend() external profitDividendDay  {
+        require(msg.sender == managerAddress, "This function can be called from manager only.");
+        if(isCarExpensesPaid && isDriverSalaryPaid){
+            // Ödemeler yapıldıktan sonra kontratta kalan paradan
+            // ilk giriş ücretlerini çıkarırsak kalan kar olur.
+            profitDividendLastPaymentDay = block.timestamp;
+            profit = address(this).balance - (participantsCount * participationFee);
+            uint dividend = profit / participantsCount;
+            for (uint i = 1; i < participantsBalances.length; i++) {
+                participantsBalances[i] += dividend;
+            }
+            if(profit <= 0) {
+                revert("There is no profit because no one used our taxi");
+            }
+        }
+    }
+    function getDividend() external {
+        require(isParticipant[msg.sender], "You did not participate in this contract.");
+        if(participantsBalances[participantIndex[msg.sender]] == 0){
+            revert("You have no money in your account");
+        }
+        msg.sender.transfer(participantsBalances[participantIndex[msg.sender]]);
+        participantsBalances[participantIndex[msg.sender]] = 0;
     }
 }
